3caea21aa014c71dcd27206ae3f50994aa331342
Jeffrey Blattman <jeff@clover.com>
2013-10-07 17:23:50 -0700

    up precision

--- NinjaTempProject/NinjaTemp/src/main/java/org/jtb/ninjatemp/Period.java
+++ NinjaTempProject/NinjaTemp/src/main/java/org/jtb/ninjatemp/Period.java
@@ -5,7 +5,7 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 enum Period {
-  ONE_HOUR(60 * 60 * 1000, "1min"), SIX_HOURS(6 * 60 * 60 * 1000, "1min"), TWELVE_HOURS(12 * 60 * 60 * 1000, "2min"), ONE_DAY(1 * 24 * 60 * 60 * 1000, "4min"), TWO_DAYS(2 * 24 * 60 * 60 * 1000, "8min"), ONE_WEEK(7 * 24 * 60 * 60 * 1000, "30min");
+  ONE_HOUR(60 * 60 * 1000, "1min"), SIX_HOURS(6 * 60 * 60 * 1000, "1min"), TWELVE_HOURS(12 * 60 * 60 * 1000, "1min"), ONE_DAY(1 * 24 * 60 * 60 * 1000, "2min"), TWO_DAYS(2 * 24 * 60 * 60 * 1000, "8min"), ONE_WEEK(7 * 24 * 60 * 60 * 1000, "30min");
 
   final long timeMillis;
   final String interval;
